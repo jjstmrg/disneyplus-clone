504342ce9500545bcf7ab370ad962e4347ea83cf
jjstmrg <johanesjs@gmail.com>
2022-02-27 22:59:20 +0700

    add imgslider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
 
 function Home() {
   return (
@@ -16,6 +17,7 @@ const Container = styled.main`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
     position: relative;
+    overflow-x: hidden;
 
     &:before{       
         background: url("/images/home-background.png") center center / cover no-repeat fixed;
